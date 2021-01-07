ad3b89c2cbff649653f0abd6978efb317dc6bbfa
Jonah Shin <jonahshin@Jonahs-MacBook-Pro.local>
2021-01-06 18:37:27 -0800

    Added App.js and styles.css

---
+++ App.js
@@ -0,0 +1,22 @@
+const options = ["Rock", "Paper", "Scissors"];
+
+function computerPlay() {
+    return options[~~(Math.random() * options.length)];
+}
+
+function playGame(cpuInput, userInput) {
+    const formatInput = userInput.charAt(0).toUpperCase() + userInput.toLowerCase().slice(1);
+    if ((formatInput === "Rock" && cpuInput === "Scissors") ||
+        (formatInput === "Scissors" && cpuInput === "Paper") ||
+        (formatInput === "Paper" && cpuInput === "Rock")) {
+        return `You win! ${formatInput} beats ${cpuInput}.`;
+    } else if ((formatInput === "Rock" && cpuInput === "Paper") ||
+        (formatInput === "Scissors" && cpuInput === "Rock") ||
+        (formatInput === "Paper" && cpuInput === "Scissors")) {
+        return `You lose! ${formatInput} loses to ${cpuInput}.`;
+    } else {
+        return "It's a tie!";
+    }
+}
+
+console.log(playGame(computerPlay(), "rOck"))
